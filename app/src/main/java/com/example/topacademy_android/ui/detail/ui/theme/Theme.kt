1fedc8a537150b77a680a5a01b1b79cdc91cdce8
Maxim <145267047+mzt-mx@users.noreply.github.com>
2025-07-29 21:36:43 +0300

    Update Theme.kt

--- app/src/main/java/com/example/topacademy_android/ui/detail/ui/theme/Theme.kt
+++ app/src/main/java/com/example/topacademy_android/ui/detail/ui/theme/Theme.kt
@@ -22,7 +22,7 @@ private val LightColorScheme = lightColorScheme(
     secondary = PurpleGrey40,
     tertiary = Pink40
 
-    /* Other default colors to override
+    /* 
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
@@ -36,7 +36,7 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun Topacademy_androidTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
+    
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
