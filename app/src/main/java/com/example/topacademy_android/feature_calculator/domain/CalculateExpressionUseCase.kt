c0479697d066e52251b68675291356b51274418c
mzt-mx <mzt-mx@example.com>
2025-09-03 21:32:30 +0300

    Исправил CackParser+

--- app/src/main/java/com/example/topacademy_android/feature_calculator/domain/CalculateExpressionUseCase.kt
+++ app/src/main/java/com/example/topacademy_android/feature_calculator/domain/CalculateExpressionUseCase.kt
@@ -1,19 +1,9 @@
 package com.example.topacademy_android.feature_calculator.domain
 
 import com.example.topacademy_android.feature_calculator.data.CalculatorParser
-import java.util.Locale
 
 class CalculateExpressionUseCase(private val parser: CalculatorParser) {
-    fun execute(expression: String): String {
-        return try {
-            val result = parser.eval(expression)
-            if (result % 1.0 == 0.0) {
-                result.toInt().toString()
-            } else {
-                String.format(Locale.US, "%.2f", result)
-            }
-        } catch (_: Exception) {
-            "Ошибка"
-        }
+    fun execute(expression: String): Double {
+        return parser.eval(expression)
     }
 }
--- app/src/main/java/com/example/topacademy_android/feature_calculator/presentation/CalculatorViewModel.kt
+++ app/src/main/java/com/example/topacademy_android/feature_calculator/presentation/CalculatorViewModel.kt
@@ -4,12 +4,22 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.topacademy_android.feature_calculator.domain.CalculateExpressionUseCase
+import java.util.Locale
 
 class CalculatorViewModel(private val calculateUseCase: CalculateExpressionUseCase) : ViewModel() {
     private val _result = MutableLiveData<String>()
     val result: LiveData<String> = _result
 
     fun onCalculate(expression: String) {
-        _result.value = calculateUseCase.execute(expression)
+        try {
+            val result = calculateUseCase.execute(expression)
+            _result.value = if (result % 1.0 == 0.0) {
+                result.toInt().toString()
+            } else {
+                String.format(Locale.US, "%.2f", result)
+            }
+        } catch (e: Exception) {
+            _result.value = "Ошибка"
+        }
     }
 }
